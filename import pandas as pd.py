4122e7d38bc4881b4704406e4f99a58732041d82
UriCapdevila <Uri_CapdevilaWRK@outlook.com>
2025-08-31 16:46:41 -0300

    Commit_inicial
    Estructura de inicio del trabajo, excel y .py inicial.

---
+++ import pandas as pd.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+# Cargar ambas hojas del Excel
+archivo = r"C:\Users\Uri_C\OneDrive\Documentos\GitHub\BigData-y-PoliticasPublicas\CSV\estadisticas2025paralumnos.xlsx"  # reemplazá con la ruta real
+
+estadisticas = pd.read_excel(archivo, sheet_name="estadistica_fin_de_cursado")  # nombre real de la hoja
+cupos = pd.read_excel(archivo, sheet_name="resultados_desgranamiento")  # nombre real de la hoja
